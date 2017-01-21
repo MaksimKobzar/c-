10f72fdec5d44fb6e102255c49827466cae95f97
KobzarMaxim <wtf__wow@mail.ru>
2017-01-21 22:36:12 +0300

    add first exercices

---
+++ exercices C/exec_1/src/exec_1.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : exec_1.c
+ Author      : Maksim Kobzar
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Name \t : Maksim Kobzar");
+	puts("DOB \t : March 22, 1993");
+	puts("Mobile \t : 8-916-394-04-15");
+	return EXIT_SUCCESS;
+}
+
+
+/*
+Name   : Alexandra Abramov  
+DOB    : July 14, 1975  
+Mobile : 99-9999999999
+*/
