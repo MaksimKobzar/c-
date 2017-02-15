50032a8fa9c2c31f4a449e3134370585985ed748
MaximKobzar <wtf__wow@mail.ru>
2017-02-16 02:52:53 +0300

    mail2

--- exercices C/mail2/src/mail2.c
+++ exercices C/mail2/src/mail2.c
@@ -10,22 +10,12 @@
 #include <stdio.h>
 #include <math.h>
 
-char testArray[200][180];
-
-
 void setPoint(int xCoord, int yCoord) {
 	printf("setPoint: (%d, %d) \n", xCoord, yCoord);
-	testArray[xCoord][yCoord] = 1;
 }
 
-int getYCoord(int xCurr, int xRad, int yRad) {
-	
+int getNewY(int xCurr, int xRad, int yRad) {
 	float temp = ((float)(xCurr*xCurr) / (float)(xRad*xRad));
-
-	printf("temp %2f, sqrt %2f, int*sqrt %2f \n", temp, sqrt(1 - temp), yRad*sqrt(1 -temp));
-	printf("MY_DBG getYCoord: \t xCurr %d, xRad %d, yRad %d, Result %2f, Result %d \n",
-				xCurr, xRad, yRad, yRad * sqrt(1 - temp), ((int)( yRad * sqrt(1 - temp) )));
-	// round value
 	return ((int)( yRad * sqrt(1 - temp)  + 0.5));
 }
 
@@ -38,14 +28,12 @@ void drawEllipse(int xNumPoints, int yNumPoints, int xCen, int yCen, int xRad, i
 	}
 
 	// Initialization
-	xCurr 		= xCen + xRad; 	// 26 
-	yCurr 		= yCen; 				// 73
-	xCurrInv 	= xCen - xRad; 	// 4
-	yCurrInv 	= yCen; 				// 73
+	xCurr 		= xCen + xRad; 
+	yCurr 		= yCen; 			
+	xCurrInv 	= xCen - xRad;
+	yCurrInv 	= yCen;
 
-	printf("MY_DBG: Initial values (x0 %d, y0 %d, x1 %d, y1 %d) \n", xCurr, yCurr, xCurrInv, yCurrInv);
-
-	while(xCurr != xCen) {
+	while(xCurr >= xCen) {
 		
 		if(yCurr != yCen) {
 			setPoint(xCurr, yCurrInv);
@@ -55,7 +43,7 @@ void drawEllipse(int xNumPoints, int yNumPoints, int xCen, int yCen, int xRad, i
 		}
 		setPoint(xCurrInv++, 			yCurrInv);
 		setPoint(xCurr--, 				yCurr);
-		yCurr = getYCoord(xCurr - xCen, xRad, yRad) + yCen;
+		yCurr = getNewY(xCurr - xCen, xRad, yRad) + yCen;
 		yCurrInv = 2*yCen - yCurr;
 	}
 }
@@ -63,21 +51,6 @@ void drawEllipse(int xNumPoints, int yNumPoints, int xCen, int yCen, int xRad, i
 int main(void) {
 	int width = 200, height = 180, x0 = 55, y0 = 113, rX = 11, rY = 4; 
 
-	int i = 0, j = 0;
-	for(j = 0; j < height; j++) {
-		for(i = 0; i < width; i++) {
-			testArray[i][j] = 0;
-		}
-	}
-
 	drawEllipse(width, height, x0, y0, rX, rY);
-
-
-		for(i = 0; i < width; i++) {
-	for(j = 0; j < height; j++) {
-			printf("%c", testArray[i][j] ? 'x' : ' ' );
-		}
-		printf("\n");
-	}
 	return 0;
 }
