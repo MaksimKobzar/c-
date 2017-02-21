a18fc94caa7803d856f90fca1c2fdb33ee628ba8
MaximKobzar <wtf__wow@mail.ru>
2017-02-22 00:04:15 +0300

    compile my first project

---
+++ c++_work_directory/CLion/HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string myName;
+    std::cout << "Enter your name:";
+    std::cin >> myName;
+    std::cout << "My name is " << myName << "!\n"<< std::endl;
+    return 0;
+}
