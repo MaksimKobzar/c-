fa6a2c538a6966a7181cb7ffb015989b5beef145
MaximKobzar <wtf__wow@mail.ru>
2017-02-23 02:30:35 +0300

    work with stepik org tests

--- c++_work_directory/CLion/HelloWorld/main.cpp
+++ c++_work_directory/CLion/HelloWorld/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-int main() {
-    std::string myName;
-    std::cout << "Enter your name:";
-    std::cin >> myName;
-    std::cout << "My name is " << myName << "!\n"<< std::endl;
+int main()
+{
+    std::string out = "\0";
+    int index = 0;
+    bool wasSpace = 0;
+
+    char c = '\0';
+
+    while (cin.get(c)) {
+
+        if ((index == 0) || !((c == ' ') && wasSpace)) {
+            out.push_back(c);
+            index++;
+        }
+
+        wasSpace = c == ' ';
+
+        if(c == '\0' || c == '\n')
+            break;
+    }
+    cout << std::out << "\n" << endl;
+
     return 0;
 }
